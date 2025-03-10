2598c6b7cbfd9a667359edf4b5d25c4736b8e6cc
sajjad isvand <sajjadzibaee102@gmail.com>
2025-03-10 09:54:58 +0330

    feat(router): add query parameter handling for navigation to privacy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import FooterSection from './components/FooterSection'
 import Navbar from './components/Navbar'
 import Donate from './pages/Donate'
@@ -7,12 +7,23 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 
+function QueryParamHandler() {
+	const location = useLocation()
+	const queryParams = new URLSearchParams(location.search)
+	const page = queryParams.get('page')
+	if (page === 'privacy') {
+		return <Navigate to="/privacy" replace />
+	}
+
+	return <Home />
+}
+
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<QueryParamHandler />} />
 				<Route path="/donate" element={<Donate />} />
 				<Route path="/extension" element={<Extension />} />
 				<Route path="/privacy" element={<PrivacyPolicy />} />
