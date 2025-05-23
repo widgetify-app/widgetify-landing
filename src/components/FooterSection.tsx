ce28cd355191beee0396fe2211838c50dd202deb
sajjad isvand <sajjadzibaee102@gmail.com>
2025-05-23 14:35:15 +0330

    fix(FooterSection): remove unused FaX icon import for cleaner code

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,5 +1,5 @@
 import { FaDiscord, FaGithub, FaInstagram, FaTelegram } from 'react-icons/fa'
-import { FaTwitter, FaX } from 'react-icons/fa6'
+import { FaTwitter } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 import {
 	DESKTOP_LINK,
